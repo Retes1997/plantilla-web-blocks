e9d3804f2a1e027283c078c74b670a051969217c
luisretes <luisretesdesign@gmail.com>
2025-09-09 21:43:55 -0500

    primer commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// src/main.js
+
+// Importamos todos los estilos en el orden correcto
+import './css/reset.css';
+import './css/tokens.css';
+import './css/foundations.css';
+import './css/global.css';
+import './css/utilities.css';
+import './css/components.css';
+import './css/patterns.css';
+
+// Si necesitas JS extra, aquí lo agregas
+console.log("Proyecto Vite Vanilla corriendo con Design System 🎨");
